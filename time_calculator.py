30dbfbaf254a26a419d6a88f7507b951e1be7274
Josedavid Ruiz Herrera <109052525+SekiroXtreme@users.noreply.github.com>
2022-10-03 20:16:20 +0000

    Yeah Baby!!!!

--- time_calculator.py
+++ time_calculator.py
@@ -18,38 +18,45 @@ def add_time(start, duration, date = False):
   
 
   minutes = start_minutes + duration_minutes
-  if minutes >= 60 :
+  if (minutes >= 60) :
     start_hours+= 1
     minutes = minutes % 60
   amount_of_am_or_pm = int((start_hours + duration_hours) / 12)
   hours = (start_hours + duration_hours) % 12
-  amount_of_days = int(duration_hours // 24)
+  amount_of_days = int(duration_hours / 24)
   
   if minutes > 9 :
     minutes = minutes
   else:
     minutes = "0" + str(minutes)
 
+  if hours == 0:
+    hours = 12
+  else:
+    hours = hours
+
+  if AM_or_PM == "PM" and start_hours + (duration_hours % 12) >= 12 :
+    amount_of_days+= 1
+
   if amount_of_am_or_pm % 2 == 1 :
     AM_or_PM = AM_PM_flip[AM_or_PM]
   else:
     AM_or_PM = AM_or_PM
 
-  if (AM_or_PM == "PM" and start_hours + (duration_hours % 12) >= 12 ):
-    amount_of_days+= 1
-
   Format_time= str(hours) + ":" + str(minutes) + " " + AM_or_PM
 
   if (date) :
-    days_of_the_week = day_of_the_week.lower()
-    index = int((days_of_the_week_ind[day_of_the_week] + amount_of_days) % 7
+    days_of_the_week = date.lower()
+    index = int((day_of_the_week_ind[days_of_the_week] + amount_of_days) % 7)
     new_day = day_of_the_week[index]
     Format_time+= ", " + new_day
 
   if (amount_of_days == 1):
     return Format_time + " " + "(next day)"
   elif (amount_of_days > 1):
-    return Format_time + " " + "(" + str(amount_of_days) + " days later)"
+    return Format_time + " (" + str(amount_of_days) + " days later)"
+
+  return Format_time
     
     
   
